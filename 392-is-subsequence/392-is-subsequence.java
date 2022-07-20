72961e79b694b26f9fe173420549c90ef3b8c70e
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-07-20 22:01:00 +0530

    Time: 1 ms (93.01%), Space: 42.3 MB (25.49%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,31 +1,49 @@
-class Solution {
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+        
+//         int count=0;
+//         int j;
+//         int k=0;
+//         for(int i=0;i<s.length();i++)
+//         {
+//             for(j=k;j<t.length();j++)
+//             {
+//                 if(t.charAt(j)==s.charAt(i))
+//                 {
+//                     count++;
+//                     k=j+1;
+//                     break;
+//                 }
+//                 else
+//                 {
+//                     if(j==t.length()-1)
+//                         return false;
+//                 }
+//             }
+//         }
+        
+//         if(s.length()==count)
+//             return true;
+//         else
+//             return false;
+        
+//     }
+// }
+
+
+// Two pointer approach
+
+public class Solution {
     public boolean isSubsequence(String s, String t) {
-        
-        int count=0;
-        int j;
-        int k=0;
-        for(int i=0;i<s.length();i++)
-        {
-            for(j=k;j<t.length();j++)
-            {
-                if(t.charAt(j)==s.charAt(i))
-                {
-                    count++;
-                    k=j+1;
-                    break;
-                }
-                else
-                {
-                    if(j==t.length()-1)
-                        return false;
-                }
+        if (s.length() == 0) return true;
+        int indexS = 0, indexT = 0;
+        while (indexT < t.length()) {
+            if (t.charAt(indexT) == s.charAt(indexS)) {
+                indexS++;
+                if (indexS == s.length()) return true;
             }
+            indexT++;
         }
-        
-        if(s.length()==count)
-            return true;
-        else
-            return false;
-        
+        return false;
     }
 }
