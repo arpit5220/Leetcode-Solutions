fb45c6efe1841cfa334d4af8b486172bdfa9899c
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-07-23 01:30:32 +0530

    Time: 19 ms (35.08%), Space: 68 MB (76.25%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+//         // USING HASHMAP
+//         HashMap<Integer,Integer> hm =new HashMap<>();
+        
+//         for(int i:nums)
+//         {
+//             if(hm.containsKey(i))
+//                 return true;
+//             else
+//                 hm.put(i,1);
+//         }
+        
+//         return false;
+        
+        
+        // USING HASHSET
+        
+        HashSet<Integer> set=new HashSet<>();
+        
+        for(int val:nums)
+        {
+            if(set.contains(val))
+                return true;
+            
+            set.add(val);
+        }
+        
+        return false;
+    
+        
+    }
+}
