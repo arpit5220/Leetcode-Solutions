e9c53a2888b96eb2db1d6405dc24c62d0cea5601
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-09-06 23:44:10 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (67.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -91,7 +91,8 @@
 
 
 
-class Solution {
+// USING TABULATION
+/*class Solution {
    public int fib ( int n ) {
        if ( n<=1 ) return n;
 
@@ -104,4 +105,25 @@ class Solution {
        }
        return dp [ n ]; 
    }
+} */
+
+
+// USING SPACE OPTIMAIZATION
+
+class Solution {
+   public int fib ( int n ) {
+       
+       if (n<=1) return n;
+
+       int prev1=0 , prev2 =1 ;
+       for (int i=2;i<=n;i++) {
+           int curr=prev1+prev2;
+           prev1 = prev2;
+           prev2 = curr;
+       }
+       return prev2;
+   }
 }
+                     
+
+
