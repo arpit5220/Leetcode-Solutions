dd2f0829c0f05da8656666eadd6bbfe600faef84
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-09-12 22:42:48 +0530

    Time: 6 ms (34.25%), Space: 67 MB (80.52%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean canJump(int[] jumps) {
         
-        int reach=0;
-        for(int i=0;i<jumps.length;i++){
+        int reach=0;                        // time --> o(n)
+        for(int i=0;i<jumps.length;i++){    // space --> o(1)
             if(i>reach) return false;
             
             if(i+jumps[i]>reach)
