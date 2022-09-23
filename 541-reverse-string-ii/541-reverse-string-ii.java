f1c595f5b1511dc3f35bd327c937391d04a4554c
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-09-23 09:21:20 +0530

    Time: 7 ms (15.34%), Space: 44.2 MB (35.58%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.java
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -1,60 +1,63 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        
-        
-        int p=k;
-        int j=0;
-        StringBuilder strb=new StringBuilder();
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<s.length();i++)
-        {
+    
+        // My solution space -O(n) , time-> 0(n)
+//         int p=k;
+//         int j=0;
+//         StringBuilder strb=new StringBuilder();
+//         StringBuilder str=new StringBuilder();
+//         for(int i=0;i<s.length();i++)
+//         {
            
-            if(p==0)
-            {
-                if(j!=1){
-                strb.reverse();
-                str.append(strb);
-                strb.setLength(0);
-                    j=1;
-                }
-                else{
-                j=0;
-                }
+//             if(p==0)
+//             {
+//                 if(j!=1){
+//                 strb.reverse();
+//                 str.append(strb);
+//                 strb.setLength(0);
+//                     j=1;
+//                 }
+//                 else{
+//                 j=0;
+//                 }
                 
-                 p=k;
+//                  p=k;
+//             }
+            
+//                 if(j==0){
+//                     strb.append(s.charAt(i)+"");
+//                     p--;
+//                     }
+//                 if(j==1){
+//                     str.append(s.charAt(i)+"");
+//                     p--;
+//                 }    
+            
+//         }
+        
+//         strb.reverse();
+//         str.append(strb);
+//         return str.toString();
+        
+        
+        char [] array =s.toCharArray();
+        for(int i=0;i<s.length();i=i+(2*k))
+        {
+            int start=i;
+            int end=Math.min(i+k, s.length()) -1;
+            char temp;
+            
+            while(start<end)
+            {
+                temp=array[start];
+                array[start]=array[end];
+                array[end]=temp;
+                start++;
+                end--;
             }
-            
-                if(j==0){
-                    strb.append(s.charAt(i)+"");
-                    p--;
-                    }
-                if(j==1){
-                    str.append(s.charAt(i)+"");
-                    p--;
-                }    
-            
         }
         
-        
-        
-        strb.reverse();
-        str.append(strb);
-        
-        return str.toString();
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return new String(array);
         
     }
 }
