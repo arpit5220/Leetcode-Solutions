744fab44d6749914f944b38ec5ce5ce23836176c
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-09-30 01:17:00 +0530

    Time: 59 ms (5.62%), Space: 40.6 MB (79.61%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        
+        int count=0;
+        for(int i=2;i<=n-2;i++)
+        {
+            int num=convert(n,i);
+            if(palindrome(num)) count++;
+        }
+        
+        if(count==n-3) return true;
+        else
+            return false;
+        
+        
+        
+    }
+    
+    public static boolean palindrome(int num)
+    {
+        int rev=0;
+        
+        while(num>0)
+        {
+            rev=rev*10+num%10;
+            num=num/10;
+        }
+        
+        if(num==rev)
+            return true;
+        else
+            return false;
+    }
+    
+    
+    public static int convert(int n,int base)
+    {
+        
+        int p=1;
+        int rv=0;
+        
+        while(n>0)
+        {
+            int digit=n%base;
+            n=n/base;
+            
+            rv+=digit*p;
+            p=p*10;
+        }
+        
+        return rv;
+    }
+}
