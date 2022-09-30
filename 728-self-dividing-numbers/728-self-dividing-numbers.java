2354ef4a96e2991c0e0792a107b1dead76f9019b
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-09-30 13:48:22 +0530

    Time: 5 ms (43.57%), Space: 41.3 MB (84.69%) - LeetHub

---
+++ 728-self-dividing-numbers/728-self-dividing-numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList();
+        for (int n = left; n <= right; ++n) {
+            if (selfDividing(n)) ans.add(n);
+        }
+        return ans;
+    }
+    
+     public boolean selfDividing(int n) {
+        int x = n;
+        while (x > 0) {
+            int d = x % 10;
+            x /= 10;
+            if (d == 0 || (n % d) > 0) return false;
+        }
+        return true;
+     }
+
+ }
