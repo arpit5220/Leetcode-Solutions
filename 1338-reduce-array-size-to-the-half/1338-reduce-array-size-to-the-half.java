a1c97cb1e0b1e795c3baa451f9363698811652d2
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2022-10-27 18:19:40 +0530

    Time: 155 ms (13.08%), Space: 96.6 MB (17.18%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -3,12 +3,11 @@ class Solution {
          Map<Integer, Integer> m = new HashMap<>();
         for (int n : a) 
             m.put(n, m.getOrDefault(n, 0) + 1);
-        PriorityQueue<Integer> pq =
-            new PriorityQueue<>((c, d) -> d - c); 
-        for (int n : m.keySet()) pq.offer(m.get(n));  // offer used to add in priorityqueue
+        PriorityQueue<Integer> pq =new PriorityQueue<>(Collections.reverseOrder());  // Max priority Queue
+        for (int n : m.keySet()) pq.offer(m.get(n));  // offer used to add element in  priority queue
         int res = 0, sum = 0;
         while(!pq.isEmpty()){
-            sum += pq.poll();   // poll used to get element and delete at at same time
+            sum += pq.poll();   // poll used to get element and delete at at same time from priority queue
             res++;
             if (sum >= (a.length + 1) / 2) return res;
         }
