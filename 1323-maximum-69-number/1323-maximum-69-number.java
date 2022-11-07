776a2880523ca33508e78afe6fd7a95a97a25932
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2022-11-07 18:45:50 +0530

    Time: 1 ms (88.30%), Space: 40.5 MB (75.39%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String s=String.valueOf(num);
+        char c[]=s.toCharArray();
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]=='6')
+            {
+                c[i]='9';
+                break;
+            }
+        }
+        return Integer.parseInt(String.valueOf(c));
+    }
+}
