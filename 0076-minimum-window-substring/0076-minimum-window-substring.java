7efce4855f89ccd4b8493d54937a83c6ced18719
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2022-11-27 16:10:46 +0530

    Time: 619 ms (5.03%), Space: 441.5 MB (5.00%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,72 +1,73 @@
 class Solution {
-    public String minWindow(String s, String t) {
-
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
+    public String minWindow(String s1, String s2) {
+        
+        String ans="";
+        
+        HashMap<Character,Integer> map2=new HashMap<>();
+        for(char ch:s2.toCharArray())
+        {
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
         }
-
-        // Dictionary which keeps a count of all the unique characters in t.
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-        for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
-        }
-
-        // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
-
-        // Left and Right pointer
-        int l = 0, r = 0;
-
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
-
-        // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-
-        // ans list of the form (window length, left, right)
-        int[] ans = { -1, 0, 0 };
-
-        while (r < s.length()) {
-            // Add one character from the right to the window
-            char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            // If the frequency of the current character added equals to the
-            // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
-            }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = s.charAt(l);
-                // Save the smallest window until now.
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+        
+        int mct=0; // match count
+        int dmct=s2.length(); // desired match count
+        
+        HashMap<Character,Integer> map1=new HashMap<>();
+        
+        int i=-1;
+        int j=-1;
+        
+        while(true)
+        {
+            boolean f1=false;
+            boolean f2=false;
+            
+            // acquire
+            while(i<s1.length()-1 && mct<dmct)
+            {
+                i++;
+                char ch=s1.charAt(i);
+                map1.put(ch,map1.getOrDefault(ch,0)+1);
+                
+                if(map1.getOrDefault(ch,0) <= map2.getOrDefault(ch,0))
+                {
+                    mct++;
                 }
-
-                // The character at the position pointed by the
-                // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
-                }
-
-                // Move the left pointer ahead, this would help to look for a new window.
-                l++;
+                f1=true;
             }
-
-            // Keep expanding the window once we are done contracting.
-            r++;
+            
+            // collect ans and release
+            while(j<i && mct==dmct)
+            {
+                String pans=s1.substring(j+1,i+1);
+                if(ans.length()==0 || pans.length()<ans.length() )
+                {
+                    ans=pans;
+                }
+                j++;
+                
+                char ch=s1.charAt(j);
+                if(map1.get(ch)==1)
+                    map1.remove(ch);
+                else
+                {
+                    map1.put(ch,map1.get(ch)-1);     
+                }
+                
+                if(map1.getOrDefault(ch,0) < map2.getOrDefault(ch,0))
+                {
+                    mct--;
+                }
+                f2=true;
+            }
+            
+            if(f1==false && f2==false)
+            {
+                break;
+            }
+            
         }
-
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        
+        return ans;     
     }
 }
