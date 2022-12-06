9e0019dc116790b3a3a7e43c5eddd2954cd84f74
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2022-12-06 23:56:01 +0530

    Update 0007-reverse-integer.java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,7 +1,7 @@
 class Solution{
 public int reverse(int x) {
         long res = 0;
-        while (x != 0) {
+        while (x!= 0) {
             res = res * 10 + x % 10;
             x = x / 10;
         }
