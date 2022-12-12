98f8dac560e2ae9ce69a5e933a16c73f9494f50e
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2022-12-12 23:19:34 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (74.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int [] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=1;
+        dp[1]=1;
+        return climb(n,dp);
+    }
+    
+    public static int climb(int n,int []dp)
+    {
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        
+        int a=(n-1>=0) ? climb(n-1,dp):0;
+        int b=(n-2>=0) ? climb(n-2,dp):0;
+          dp[n]=a+b;
+        
+        return dp[n];
+    }
+}
