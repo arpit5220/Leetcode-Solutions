2870d90915ffdc93c1ead4051de5d377b999daa6
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-02-04 15:01:41 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (94.92%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,19 +14,47 @@
  * }
  */
 class Solution {
-    int count=0;
+ //   int count=0;
     
     public int countNodes(TreeNode root) {
+        
+//         if(root==null)
+//             return 0;     // -> O(n) approach
+        
+//         countNodes(root.left);
+//         countNodes(root.right);
+//         count++;
+        
+//         return count;
         
         if(root==null)
             return 0;
         
-        countNodes(root.left);
-        countNodes(root.right);
-        count++;
+        int lh=0;
+        TreeNode node=root.left;
+        while(node!=null)
+        {
+            node=node.left;
+            lh++;
+        }
         
-        return count;
+        int rh=0;
+        node=root.right;
+        while(node!=null)
+        {
+            node=node.right;
+            rh++;
+        }
         
+        if(lh==rh)
+        {
+            int ht=lh+1;
+            return (1<<ht)-1;
+        }
+        else
+        {
+            return countNodes(root.left)+countNodes(root.right)+1;
+        }
         
     }
 }
