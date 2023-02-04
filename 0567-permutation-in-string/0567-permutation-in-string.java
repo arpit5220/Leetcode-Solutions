a3f30cc244c661b69e4e439183dc6b21c0d994e2
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-02-05 00:09:09 +0530

    Time: 125 ms (25.05%), Space: 42.7 MB (45.02%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        int [] arr1=new int[26];
+       // int [] arr2=new int[26];
+        
+        if(s1.length()> s2.length())
+            return false;
+        
+        int len1=s1.length();
+        for(int i=0;i<s1.length();i++)
+        {
+            arr1[s1.charAt(i)-'a']++;
+        }
+       
+        int j=0;
+        while(j<=s2.length()-len1)
+        {
+            int [] arr2=new int[26];
+            int k=j;
+            for(int i=0;i<len1;i++)
+            {
+                arr2[s2.charAt(k++)-'a']++;
+            }
+            
+            if(Arrays.equals(arr1,arr2))
+            {
+                return true;
+            }
+            else
+            {
+                j++;
+                
+            }
+        }
+        
+        return false;
+    }
+}
