be1efe050a9e72cce69f0fe6284dc9d1ba155636
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-03-02 00:47:15 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (20.99%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,32 +24,84 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
-        if(root==null)
-            return root;
+//         if(root==null)
+//             return root;
         
-        Queue<Node> pq=new ArrayDeque<>();
-        pq.add(root);
+        // 1ST APPROACH USING BFS
         
-        while(pq.size()>0)
-        {
-           // Node top=pq.remove();
-            int counter=pq.size();
-            Node prev=null;
-            for(int i=0;i<counter;i++)
-            {
-                Node top=pq.remove();
-                top.next=prev;
-                prev=top;
+//         Queue<Node> pq=new ArrayDeque<>();
+//         pq.add(root);
+        
+//         while(pq.size()>0)
+//         {
+//            // Node top=pq.remove();
+//             int counter=pq.size();
+//             Node prev=null;
+//             for(int i=0;i<counter;i++)
+//             {
+//                 Node top=pq.remove();
+//                 top.next=prev;
+//                 prev=top;
                
-                if(top.right!=null)
-                    pq.add(top.right);
+//                 if(top.right!=null)
+//                     pq.add(top.right);
                 
-                if(top.left!=null)
-                    pq.add(top.left);
+//                 if(top.left!=null)
+//                     pq.add(top.left);
                 
-            }
-        }
+//             }
+//         }
         
+//         return root;
+        
+        // 2ND APPROACH  // without space
+        
+       if(root==null)
+           return null;
+        Node curr=root;
+        Node head=null;
+        Node tail=null;
+        
+        while(curr!=null)
+        {
+            while(curr!=null)
+            {
+                if(curr.left!=null)
+                {
+                    if(head==null)
+                    {
+                        head=curr.left;
+                        tail=curr.left;
+                    }
+                    else
+                    {
+                        tail.next=curr.left;
+                        tail=curr.left;
+                    }
+                }
+                
+                if(curr.right!=null)
+                {
+                    if(head==null)
+                    {
+                        head=curr.right;
+                        tail=curr.right;
+                    }
+                    else
+                    {
+                        tail.next=curr.right;
+                        tail=curr.right;
+                    }
+                }
+                
+                curr=curr.next;
+            }
+            
+            curr=head;
+            head=null;
+            tail=null;
+        }
+      
         return root;
         
     }
