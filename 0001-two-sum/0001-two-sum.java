984dc9195d1a2ba0a4e0afabd5b1c55bd467b32f
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-07-02 13:09:58 +0530

    Time: 4 ms (63.16%), Space: 43.8 MB (40.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
+        int n=nums.length;
         HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<n;i++)
         {
             hm.put(nums[i],i);
         }
         
-        for(int i=0;i<nums.length;i++)
-        {
-            int t=nums[i];
-            int rem=target-t;
-            if(hm.containsKey(rem))
-            {
-                int index=hm.get(rem);
-                if(index==i) continue;    // to chek if 6-3=3--> already present in hashmap
-                return new int[]{i,index};  // just dry run second test case and you will be able to know
-                    
-            }
-        }
+       for(int i=0;i<n;i++)
+       {
+           int val=target-nums[i];
+           
+           if(hm.containsKey(val))
+           {
+               int index=hm.get(val);
+               if(index==i) continue;
+               return new int[] {index,i};
+           }
+       }
         
         return new int[]{};
+        
     }
 }
