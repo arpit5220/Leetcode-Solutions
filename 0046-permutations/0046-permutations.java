0f3251e26e1fcc28bf8bc95bc966245050a8db40
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-08-13 09:23:26 +0530

    Time: 1 ms (94.96%), Space: 43.9 MB (68.95%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,71 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-       
+  
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ds=new ArrayList<>();
-        boolean [] freq=new boolean[nums.length];
-        solve(nums,ans,ds,freq);
+        solve(0,nums,ans);
         return ans;
     }
-    
-    public void solve(int [] nums,List<List<Integer>> ans,List<Integer> ds,boolean[] freq)
+        
+     
+    public void solve(int index,int [] nums,List<List<Integer>> ans)
     {
-        if(ds.size()==nums.length)
+        if(index==nums.length)
         {
-            ans.add(new ArrayList<>(ds));
+            List<Integer> ds=new ArrayList<>();
+            for(int val:nums)
+            {
+                ds.add(val);
+            }
+            ans.add(ds);
             return;
         }
         
-        for(int i=0;i<nums.length;i++)
+        for(int i=index;i<nums.length;i++)
         {
-            if(freq[i]==false)
-            {
-                freq[i]=true;
-                ds.add(nums[i]);
-                solve(nums,ans,ds,freq);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+            swap(i,index,nums);
+            solve(index+1,nums,ans);
+            swap(i,index,nums);
         }
     }
+        
+      public void swap(int a,int b,int [] nums)
+      {
+          int temp=nums[a];
+          nums[a]=nums[b];
+          nums[b]=temp;
+      }
+        
+
 }
+        
+        
+        
+        // USING O(N) SPACE
+//         List<List<Integer>> ans=new ArrayList<>();
+//         List<Integer> ds=new ArrayList<>();
+//         boolean [] freq=new boolean[nums.length];
+//         solve(nums,ans,ds,freq);
+//         return ans;
+//     }
+    
+//     public void solve(int [] nums,List<List<Integer>> ans,List<Integer> ds,boolean[] freq)
+//     {
+//         if(ds.size()==nums.length)
+//         {
+//             ans.add(new ArrayList<>(ds));
+//             return;
+//         }
+        
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(freq[i]==false)
+//             {
+//                 freq[i]=true;
+//                 ds.add(nums[i]);
+//                 solve(nums,ans,ds,freq);
+//                 ds.remove(ds.size()-1);
+//                 freq[i]=false;
+//             }
+//         }
+//     }
+// }
