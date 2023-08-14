c2515acaf5ad4c4c65aaf385f21b5d1c7ef3190a
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-08-14 13:27:50 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (97.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -126,7 +126,8 @@
 } */
 
 
-class Solution {
+// USING RECURSION
+/*class Solution {
    public int fib ( int n ) {
                                   // using recursion
        if(n<=1)
@@ -135,7 +136,84 @@ class Solution {
        
        
    }
+} */
+
+// USING MEMOIZATION
+
+/*class Solution {
+   public int fib ( int n ) {
+       
+       int [] dp=new int[n+1];
+       Arrays.fill(dp,-1);
+       return fibo(n,dp);
+      
+       
+   }
+    
+  public int fibo(int n,int[] dp)
+  {
+      if(n<=1)
+          return n;
+      if(dp[n]!=-1)
+          return dp[n];
+      
+      int fib1=fibo(n-1,dp);
+      int fib2=fibo(n-2,dp);
+      dp[n]=fib1+fib2;
+      
+      return dp[n];
+  }
+    
+}*/
+
+// USING TABULATION
+
+// class Solution {
+//    public int fib ( int n ) {
+       
+//        if(n<=1) return n;
+       
+//        int dp[]=new int[n+1];
+       
+//        dp[0]=0;
+//        dp[1]=1;
+       
+//        for(int i=2;i<=n;i++)
+//        {
+//            dp[i]=dp[i-1]+dp[i-2];
+//        }
+       
+//        return dp[n];
+//    }
+// }
+
+class Solution {
+   public int fib ( int n ) {
+       
+       // Using Memoization
+       int [] dp=new int[n+1];
+       Arrays.fill(dp,-1);
+       int cal=solve(n,dp);
+       return cal;
+   }
+    
+    public int solve(int n,int [] dp)
+    {
+        if(n==1)
+            return 1;
+        if(n==0)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        int prev1=solve(n-1,dp);
+        int prev2=solve(n-2,dp);
+        dp[n]=prev1+prev2;
+        return dp[n];
+    }
 }
+
+
                      
 
 
