8d6bf11884ff6b9c14cfa6208f93df9ee4cfa890
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-08-22 21:51:09 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (97.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         
-        int [] dp=new int[n+1];
+        int [] dp=new int[n+1];  // just like fibonacci
         Arrays.fill(dp,-1);
         dp[0]=1;
         dp[1]=1;
@@ -13,9 +13,11 @@ class Solution {
         if(n==0 || n==1) return 1;
         if(dp[n]!=-1) return dp[n];
         
-        int a=(n-1>=0) ? climb(n-1,dp):0;
-        int b=(n-2>=0) ? climb(n-2,dp):0;
-          dp[n]=a+b;
+        // int a=(n-1>=0) ? climb(n-1,dp):0;
+        // int b=(n-2>=0) ? climb(n-2,dp):0;
+        
+        int val=climb(n-1,dp)+climb(n-2,dp);
+          dp[n]=val;
         
         return dp[n];
     }
