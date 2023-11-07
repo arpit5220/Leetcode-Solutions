cfb65d960e56cc2747b3ca93ecb28b471024639f
ARPIT KATIYAR <arpitkatiyar09197@gmail.com>
2023-11-08 00:19:44 +0530

    Time: 6 ms (46.35%), Space: 57 MB (48.69%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -11,57 +11,56 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-         if(head.next==null) return true;;
+        ListNode mid=middle(head);
+        ListNode second=reverse(mid.next);
+        mid.next=null;
         
-         ListNode mid=findmid(head); 
-         ListNode second=reverse(mid.next);
-        
-        while(head != null && second != null){
-            if(head.val != second.val) return false;
-            head = head.next;
-            second = second.next;
+        while(head!=null && second!=null)
+        {
+            if(head.val!=second.val) return false;
+            head=head.next;
+            second=second.next;
         }
+        
         return true;
-             
-         
+        
+        
     }
+    
+    
+    public ListNode middle(ListNode node)
+    {
+        ListNode slow=node;
+        ListNode fast=node;
+        ListNode prev=null;
+        
+        while(fast!=null && fast.next!=null)
+        {
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        if(fast==null)
+            return prev;
+        return slow;
+    }
+    
     
     
     public ListNode reverse(ListNode head)
     {
-        ListNode curr=head;
-        ListNode prev=null;
+         ListNode prev=null;
+         ListNode curr=head;
         
         while(curr!=null)
         {
-            ListNode ahead=curr.next;
-            
-            curr.next=prev;
+            ListNode ahead=curr.next;       
+            curr.next=prev; 
             prev=curr;
             curr=ahead;
         }
         
         return prev;
-    }
-    
-    public ListNode findmid(ListNode head)
-    {
-        ListNode slow=head;
-        ListNode fast=head; 
-        ListNode prev=head;
-        
-        while(fast!=null && fast.next!=null)
-        {
-             prev=slow;
-             slow=slow.next;
-             fast=fast.next.next;
-        }
-        
-        if(fast==null)
-        {
-            return prev;
-        }
-        else
-            return slow;
     }
 }
